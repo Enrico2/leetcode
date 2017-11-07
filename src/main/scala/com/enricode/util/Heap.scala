b89b7e440dc0fdd88c7543ad13b9a5a5d1a9c1b9
Ran Magen <enricomagen@gmail.com>
2017-11-07 11:19:32 -0800

    Might still have problems

--- src/main/scala/com/enricode/util/Heap.scala
+++ src/main/scala/com/enricode/util/Heap.scala
@@ -4,31 +4,33 @@ import scala.annotation.tailrec
 import scala.collection.mutable
 
 class MaxHeap[T <% Ordered[T]] extends Heap[T](_ > _)
+
 class MinHeap[T <% Ordered[T]] extends Heap[T](_ <= _)
 
-/**
-  * This might still have some errors somewhere.
-  * @param compare
-  * @param ev$1
-  * @tparam T
-  */
 abstract class Heap[T <% Ordered[T]](compare: (T, T) => Boolean) {
   private[this] val a = mutable.ArrayBuffer[T]()
 
   private[this] def left(n: Int) = 2 * n + 1
-
   private[this] def right(n: Int) = 2 * n + 2
-
   private[this] def parent(n: Int) = if (n % 2 == 0) n / 2 - 1 else n / 2
 
   def size = a.size
   def length = a.length
-
   def peek(): Option[T] = a.headOption
 
   def push(t: T): Unit = {
     a.append(t)
     bubbleUp(a.length - 1)
+  }
+
+  def pop(): Option[T] = {
+    if (a.nonEmpty) {
+      val res = a(0)
+      a(0) = a(length - 1)
+      a.remove(length - 1)
+      bubbleDown(0)
+      Some(res)
+    } else None
   }
 
   private[this] def swap(i: Int, j: Int) = {
@@ -78,15 +80,5 @@ abstract class Heap[T <% Ordered[T]](compare: (T, T) => Boolean) {
         bubbleDown(s)
       }
     }
-  }
-
-  def pop(): Option[T] = {
-    if (a.nonEmpty) {
-      val res = a(0)
-      a(0) = a(length-1)
-      a.remove(length-1)
-      bubbleDown(0)
-      Some(res)
-    } else None
   }
 }

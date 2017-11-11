5d86689812f69b83fae92b88fd2cf77c602aad80
Ran Magen <enricomagen@gmail.com>
2017-11-10 16:20:00 -0800

    ValidParenthesis

---
+++ src/main/scala/com/enricode/leetcode/ValidParenthesis.scala
@@ -0,0 +1,39 @@
+package com.enricode.leetcode
+
+import com.enricode.util.LeetcodeApp
+import scala.collection.mutable
+
+/**
+  * https://leetcode.com/problems/valid-parentheses/description/
+  */
+object ValidParenthesis extends LeetcodeApp {
+
+  override def run(): Unit = {
+    val ss = Seq("]", "(", "()", "({)}","({[][]()}({})[])") // false, true, false, true
+    ss.foreach(s => println(isValid(s)))
+  }
+
+  def isValid(str: String): Boolean = {
+    val stack = mutable.Stack[Char]() // ()
+
+    def flip(c: Char) = c match {
+      case ')' => '('
+      case ']' => '['
+      case '}' => '{'
+    }
+
+    val r = str.foldLeft(true) { (b, c) =>
+      if (!b) b else {
+        c match {
+          case ')' | ']' | '}' =>
+            if (stack.isEmpty) false else stack.pop() == flip(c)
+          case c =>
+            stack.push(c)
+            true
+        }
+      }
+    }
+
+    r && stack.isEmpty
+  }
+}

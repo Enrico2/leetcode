276d4ed085147755ad857de4513bd4db1da40cbc
Ran Magen <enricomagen@gmail.com>
2017-11-19 17:15:42 -0800

    WordLadder2

---
+++ src/main/scala/com/enricode/leetcode/WordLadder2.scala
@@ -0,0 +1,74 @@
+package com.enricode.leetcode
+
+import com.enricode.util.LeetcodeApp
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
+/**
+  * https://leetcode.com/problems/word-ladder-ii/description/
+  */
+object WordLadder2 extends LeetcodeApp {
+  override def run(): Unit = {
+    val list = List("kid","tag","pup","ail","tun","woo","erg","luz","brr","gay","sip","kay","per","val","mes","ohs","now","boa","cet","pal","bar","die","war","hay","eco","pub","lob","rue","fry","lit","rex","jan","cot","bid","ali","pay","col","gum","ger","row","won","dan","rum","fad","tut","sag","yip","sui","ark","has","zip","fez","own","ump","dis","ads","max","jaw","out","btu","ana","gap","cry","led","abe","box","ore","pig","fie","toy","fat","cal","lie","noh","sew","ono","tam","flu","mgm","ply","awe","pry","tit","tie","yet","too","tax","jim","san","pan","map","ski","ova","wed","non","wac","nut","why","bye","lye","oct","old","fin","feb","chi","sap","owl","log","tod","dot","bow","fob","for","joe","ivy","fan","age","fax","hip","jib","mel","hus","sob","ifs","tab","ara","dab","jag","jar","arm","lot","tom","sax","tex","yum","pei","wen","wry","ire","irk","far","mew","wit","doe","gas","rte","ian","pot","ask","wag","hag","amy","nag","ron","soy","gin","don","tug","fay","vic","boo","nam","ave","buy","sop","but","orb","fen","paw","his","sub","bob","yea","oft","inn","rod","yam","pew","web","hod","hun","gyp","wei","wis","rob","gad","pie","mon","dog","bib","rub","ere","dig","era","cat","fox","bee","mod","day","apr","vie","nev","jam","pam","new","aye","ani","and","ibm","yap","can","pyx","tar","kin","fog","hum","pip","cup","dye","lyx","jog","nun","par","wan","fey","bus","oak","bad","ats","set","qom","vat","eat","pus","rev","axe","ion","six","ila","lao","mom","mas","pro","few","opt","poe","art","ash","oar","cap","lop","may","shy","rid","bat","sum","rim","fee","bmw","sky","maj","hue","thy","ava","rap","den","fla","auk","cox","ibo","hey","saw","vim","sec","ltd","you","its","tat","dew","eva","tog","ram","let","see","zit","maw","nix","ate","gig","rep","owe","ind","hog","eve","sam","zoo","any","dow","cod","bed","vet","ham","sis","hex","via","fir","nod","mao","aug","mum","hoe","bah","hal","keg","hew","zed","tow","gog","ass","dem","who","bet","gos","son","ear","spy","kit","boy","due","sen","oaf","mix","hep","fur","ada","bin","nil","mia","ewe","hit","fix","sad","rib","eye","hop","haw","wax","mid","tad","ken","wad","rye","pap","bog","gut","ito","woe","our","ado","sin","mad","ray","hon","roy","dip","hen","iva","lug","asp","hui","yak","bay","poi","yep","bun","try","lad","elm","nat","wyo","gym","dug","toe","dee","wig","sly","rip","geo","cog","pas","zen","odd","nan","lay","pod","fit","hem","joy","bum","rio","yon","dec","leg","put","sue","dim","pet","yaw","nub","bit","bur","sid","sun","oil","red","doc","moe","caw","eel","dix","cub","end","gem","off","yew","hug","pop","tub","sgt","lid","pun","ton","sol","din","yup","jab","pea","bug","gag","mil","jig","hub","low","did","tin","get","gte","sox","lei","mig","fig","lon","use","ban","flo","nov","jut","bag","mir","sty","lap","two","ins","con","ant","net","tux","ode","stu","mug","cad","nap","gun","fop","tot","sow","sal","sic","ted","wot","del","imp","cob","way","ann","tan","mci","job","wet","ism","err","him","all","pad","hah","hie","aim","ike","jed","ego","mac","baa","min","com","ill","was","cab","ago","ina","big","ilk","gal","tap","duh","ola","ran","lab","top","gob","hot","ora","tia","kip","han","met","hut","she","sac","fed","goo","tee","ell","not","act","gil","rut","ala","ape","rig","cid","god","duo","lin","aid","gel","awl","lag","elf","liz","ref","aha","fib","oho","tho","her","nor","ace","adz","fun","ned","coo","win","tao","coy","van","man","pit","guy","foe","hid","mai","sup","jay","hob","mow","jot","are","pol","arc","lax","aft","alb","len","air","pug","pox","vow","got","meg","zoe","amp","ale","bud","gee","pin","dun","pat","ten","mob")
+    val beginWord = "cet"
+    val endWord = "ism"
+//
+//    val list = List("ted","tex","red","tax","tad","den","rex","pee")
+//    val beginWord = "red"
+//    val endWord = "tax"
+
+    println(findLadders(beginWord, endWord, list))
+  }
+
+  def findLadders(beginWord: String, endWord: String, wordList: List[String]): List[List[String]] = {
+    if (!wordList.contains(endWord)) return Nil
+
+    val words = wordList.toSet
+    val visited = mutable.Set[String](beginWord)
+
+    def getNext(w: String): Seq[String] = {
+      val chars1 = w.toCharArray
+      val chars2 = w.toCharArray
+      val res = ListBuffer[String]()
+      for (c <- 'a' to 'z') {
+        for (i <- 0 until w.length) {
+          chars2(i) = c
+          val str = String.valueOf(chars2)
+          if (str != w && words.contains(str) && !visited(str)) {
+            res += str
+          }
+          chars2(i) = chars1(i)
+        }
+      }
+      res
+    }
+
+    val q = new mutable.Queue[mutable.LinkedHashSet[String]]()
+    q.enqueue(mutable.LinkedHashSet(beginWord))
+
+    val ans = ListBuffer[List[String]]()
+    var found = false
+    while (q.nonEmpty && !found) {
+      val layerWords = ListBuffer[String]()
+
+      for (_ <- 0 until q.size) {
+        val path = q.dequeue()
+
+        val newWords = getNext(path.last)
+
+        layerWords.append(newWords:_*)
+
+        if (newWords.contains(endWord)) {
+          ans.append(path.toList :+ endWord)
+          found = true
+        } else {
+          newWords.foreach { w => q.enqueue(path + w) }
+        }
+      }
+
+      visited ++= layerWords.toSet
+    }
+
+    ans.toList
+  }
+}
